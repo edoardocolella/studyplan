e34fa8576eba91748f0873f0c37686996dbd0ef9
Edoardo Colella <edoardocolella.it@gmail.com>
2023-02-13 18:28:44 +0100

    fix

--- server/server.js
+++ server/server.js
@@ -18,9 +18,7 @@ app.use(express.json()); // for parsing json request body
 
 // set up and enable cors
 const corsOptions = {
-  origin: 'https://study-plan-front.onrender.com',
-  methods: ["GET", "PSOT", "DELETE", "UPDATE", "PUT", "PATCH"],
-  preflightContinue: true,
+  origin: 'http://localhost:3000',
   credentials: true,
 };
 app.use(cors(corsOptions));
